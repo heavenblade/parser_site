82e9d851742d0463fbdb38ebcf1ac407343f823c
Matteo.Amatori <matteo.amatori@eurac.edu>
2019-03-27 16:07:14 +0100

    Committing start of the main site template with bootstrap addition

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Grammar
 
 # Create your views here.
 
 def homepage(request):
-    return(HttpResponse("Parser site WIP!"))
+    return(render(request = request, template_name = "main/home.html", context = {"grammars": Grammar.objects.all}))
