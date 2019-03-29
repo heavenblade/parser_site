92dd971b47560d5eccd0065415dbf9d864daf561
Matteo.Amatori <matteo.amatori@eurac.edu>
2019-03-29 15:53:46 +0100

    Added site logo, edited form help text

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,29 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
 
 class MyRegistrationForm(UserCreationForm):
     email = forms.EmailField(required = True)
 
+    def __init__(self, *args, **kwargs):
+        super(MyRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].help_text = 'Your passowrd must contain at least 8 characters and 1 number'
+
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2',
+        ]
+
+        help_texts = {
+            'username': _(''),
+            'email': _('Insert valid email'),
+        }
 
     def save(self, commit = True):
         user = super(MyRegistrationForm, self).save(commit = False)
